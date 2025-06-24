f18e6f840c5f612e879865c9e4447c1fbfa9e92e
aderssin alfredo chuchon perez <138517552+assassinx201023@users.noreply.github.com>
2025-06-23 20:19:27 -0500

    Add files via upload

---
+++ hallar suma.cs
@@ -0,0 +1,22 @@
+﻿class problema01
+{
+    static void Main(string[] args)
+    {
+        //VARIABLES
+        int n1, n2, s;
+
+        //ENTRADA DE DADOS
+        System.Console.Write("Numero 1: ");
+        n1 = int.Parse(System.Console.ReadLine());
+        System.Console.Write("Numero 2: ");
+        n2 = int.Parse(System.Console.ReadLine());
+
+        //PROCESSAMENTO
+        s = n1 + n2;
+
+        //SAIDA DE DADOS
+        System.Console.WriteLine("");
+        System.Console.WriteLine("SUMA = " + s);
+        System.Console.ReadLine();
+    }
+}
