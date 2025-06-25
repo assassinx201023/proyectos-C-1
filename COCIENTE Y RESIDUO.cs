e934f497e78a9467f2fb005b27ac160da9b5f3ef
Aderssin Alfredo Chuchon Pérez <138517552+assassinx201023@users.noreply.github.com>
2025-06-24 22:13:24 -0500

    Add files via upload

---
+++ COCIENTE Y RESIDUO.cs
@@ -0,0 +1,26 @@
+﻿    using System;
+
+class Problema02
+{
+    static void Main(string[] args)
+    {
+        //Variable
+        int n1, n2, c, r;
+
+        //Entra
+        Console.WriteLine("Numero 1: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Numero 2: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        //Proceso
+        c = n1 / n2;
+        r = n1 % n2;
+
+        //Salida
+        Console.WriteLine("");
+        Console.WriteLine("Cociente : "+ c);
+        Console.WriteLine("Residuo: "+ r);
+        Console.ReadLine();
+    }
+}
