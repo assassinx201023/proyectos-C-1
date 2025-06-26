c8b61bf1789d906d86faeee499c9338b8591beeb
Aderssin Alfredo Chuchon Pérez <138517552+assassinx201023@users.noreply.github.com>
2025-06-25 22:06:06 -0500

    Add files via upload

---
+++ IGV19%.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program03
+{
+
+    static void Main(string[] args)
+
+    {
+        //VARIABLES
+        float vv, igv, pv;
+
+        //Entrada de datos
+        Console.Write("Valor de venta: ");
+        vv = float.Parse(Console.ReadLine());
+
+        //Proceso
+        igv = vv * 0.19f;
+        pv = vv + igv;
+
+        //Salida de datos
+        Console.WriteLine(" ");
+        Console.WriteLine("IGV: " + igv);
+        Console.WriteLine("Precio de venta: " + pv);
+        Console.ReadLine();
+
+    }
+}
